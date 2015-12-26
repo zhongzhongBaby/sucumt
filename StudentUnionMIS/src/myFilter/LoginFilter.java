1c07aa02ee1226531ce6273c17c4623601346925
DuanJiashuai <duanjs1995@126.com>
2015-12-26 17:36:59 +0800

    新增登录过滤器

---
+++ StudentUnionMIS/src/myFilter/LoginFilter.java
@@ -0,0 +1,70 @@
+package myFilter;
+
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+@WebFilter("/web/*")
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		//¹ýÂËÁ´
+		
+		HttpServletRequest req = (HttpServletRequest)request;
+		
+		HttpSession session = req.getSession();
+		
+		if (
+				session.getAttribute("logined") != null && 
+				(Boolean)session.getAttribute("logined")
+		) {
+			chain.doFilter(request, response);
+		}
+		else {
+			((HttpServletResponse)response).sendRedirect("../index.jsp");
+		}
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
